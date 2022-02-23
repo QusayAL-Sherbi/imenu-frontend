792578325dc532691d45673824a31ddb70575b85
QusayAL-Sherbi <qusay.alsherbi@gmail.com>
2022-02-23 16:01:49 +0200

    add image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,6 +9,7 @@ let About = () => {
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas possimus debitis mollitia doloribus earum. Voluptatem in debitis, ullam inventore, aspernatur, repellat ipsum corrupti amet similique sequi tempore mollitia facilis velit beatae asperiores odit soluta obcaecati?
         </p>
+        <img src="https://i2.wp.com/www.business-to-you.com/wp-content/uploads/2020/03/Leadership-Management.jpg?fit=1920%2C1080&ssl=1" alt="" />
       </div>
     </section>
   )
