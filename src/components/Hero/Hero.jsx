8353c03d747f0ae03cfa65cc31cb5e0679d1f2a0
QusayAL-Sherbi <qusay.alsherbi@gmail.com>
2022-02-23 15:56:56 +0200

    add hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,9 +3,12 @@ import './Hero-style.css';
 let Hero = () => {
   return (
     <div className="hero">
+      <div className="overlay"></div>
       <div className="container">
-        <h1>iMenu</h1>
-        <p>onlile resturants menu builder</p>
+        <div className="hero-elements">
+          <h1>iMenu</h1>
+          <p>onlile resturants menu builder</p>
+        </div>
       </div>
     </div>
   )
