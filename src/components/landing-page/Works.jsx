e88be302b6812a3884d6bdaf2f1402191649fc9c
QusayAL-Sherbi <qusay.alsherbi@gmail.com>
2022-03-11 17:54:22 +0200

    Update Landing Page Pics

--- src/components/landing-page/Works.jsx
+++ src/components/landing-page/Works.jsx
@@ -6,16 +6,16 @@ const Works = () => {
       <div className="container">
         <h2>Works</h2>
         <div className="images-box">
-          <img src={require('../../images/01.jpg')} alt="Image One" />
-          <img src={require('../../images/02.jpg')} alt="Image Two" />
-          <img src={require('../../images/03.jpg')} alt="Image Three" />
-          <img src={require('../../images/04.jpg')} alt="Image Four" />
-          <img src={require('../../images/05.png')} alt="Image Five" />
-          <img src={require('../../images/06.png')} alt="Image Six" />
-          <img src={require('../../images/07.jpg')} alt="Image Seven" />
-          <img src={require('../../images/08.jpg')} alt="Image eight" />
-          <img src={require('../../images/09.jpg')} alt="Image Nine" />
-          <img src={require('../../images/10.jpg')} alt="Image Ten" />
+          <img src='https://www.fivesquid.com/pics/t2/1617381368-167764-1-1.jpg' alt="Image One" />
+          <img src='https://www.paperpapers.com/news/wp-content/uploads/2018/06/0057dd698f5fcc2.jpg' alt="Image Two" />
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQChOizx88WxMY-TJoL7iXtszqUcqfMoXFAy1kQzNU7zhcl6eXhvjJ1P1LFj7AbXB0qsY&usqp=CAU' alt="Image Three" />
+          <img src='https://cdn1.vectorstock.com/i/1000x1000/67/95/breakfast-menu-template-for-restaurant-and-cafe-vector-30246795.jpg' alt="Image Four" />
+          <img src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/74b70617145469.562b65721a67f.jpg' alt="Image Five" />
+          <img src='https://gfx4arab.com/wp-content/uploads/2020/11/Modern-restaurant-menu-for-burgers-Free-Vector.jpg' alt="Image Six" />
+          <img src='https://thumbs.dreamstime.com/z/yellow-restaurant-menu-template-186710349.jpg' alt="Image Seven" />
+          <img src='https://miro.medium.com/max/1200/1*qrn5wA1So5pkGmsLOHWSUQ.jpeg' alt="Image eight" />
+          <img src='https://st2.depositphotos.com/4312111/7268/v/950/depositphotos_72689107-stock-illustration-vintage-and-art-restaurant-menu.jpg' alt="Image Nine" />
+          <img src='https://image.shutterstock.com/image-vector/menu-restaurant-hipster-style-600w-263631041.jpg' alt="Image Ten" />
         </div>
       </div>
     </section>
